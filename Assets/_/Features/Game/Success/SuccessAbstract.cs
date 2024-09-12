45b5b8417e31eb80ba04543ba08d45bddb6d8e74
Valentin <webdev.valentin@gmail.com>
2024-09-12 10:17:59 +0200

    fix: e interaction right forward

--- Assets/_/Features/Game/Success/SuccessAbstract.cs
+++ Assets/_/Features/Game/Success/SuccessAbstract.cs
@@ -34,10 +34,19 @@ namespace Game.Runtime
 
         protected bool IsPlayerNear()
         {
-            Vector3 direction = _playerBlackboard.GetValue<Vector3>("Position") - transform.position;
-            float directionSqr = direction.sqrMagnitude;
+            Vector3 playerPosition = _playerBlackboard.GetValue<Vector3>("Position");
+            Vector3 directionToObject = transform.position - playerPosition;
 
-            if (directionSqr < 2f * 2f) return true;
+            float distanceSqr = directionToObject.sqrMagnitude;
+            if (distanceSqr > 2f * 2f) return false;
+
+            Vector3 playerForward = _playerBlackboard.GetValue<Vector3>("Forward");
+
+            directionToObject.Normalize();
+            float dotProduct = Vector3.Dot(playerForward, directionToObject);
+
+            if (dotProduct > 0.6f) return true;
+
             return false;
         }
 
