91f16336048001201e77992ff9071f9e699fdc04
Stefan <pokestef@live.be>
2024-10-08 11:50:19 +0200

    Text added for audio dialogue
    Only for triggers, not for deaths yet

--- Assets/_/Features/Game/Audios/OnTriggerAudio.cs
+++ Assets/_/Features/Game/Audios/OnTriggerAudio.cs
@@ -1,41 +1,36 @@
+using TMPro;
 using UnityEngine;
 
 namespace Game.Runtime
 {
     public class OnTriggerAudio : MonoBehaviour
     {
-        #region Publics
-
-        #endregion
-
         #region Unity
 
         private void OnTriggerEnter(Collider other)
         {
             if (other.gameObject.layer == LayerMask.NameToLayer("Player") && !_wasPlayed)
             {
+                _tmp.GetComponent<TextCleaner>().m_resetTimer = _textStayDuration;
                 _audio.clip = _clip;
                 _audio.Play();
+                _tmp.text = _audioText;
                 _wasPlayed = true;
             }
         }
 
         #endregion
 
-        #region Methods
-
-        #endregion
-
-        #region Utils
-
-        #endregion
-
         #region Privates
 
-        [SerializeField]
-        private AudioClip _clip;
-        [SerializeField]
-        private AudioSource _audio;
+        [Header ("-- Audio --")]
+        [SerializeField] private AudioClip _clip;
+        [SerializeField] private AudioSource _audio;
+
+        [Header("-- Text --")]
+        [SerializeField] private int _textStayDuration;
+        [SerializeField] private string _audioText;
+        [SerializeField] private TMP_Text _tmp;
 
         private bool _wasPlayed;
 
---
+++ Assets/_/Features/Game/Audios/TextCleaner.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+namespace Game.Runtime
+{
+    public class TextCleaner : MonoBehaviour
+    {
+        #region publics
+
+        public float m_resetTimer;
+
+        #endregion
+
+        #region UNITY API
+        private void Awake()
+        {
+            _text = GetComponent<TMP_Text>();
+        }
+        private void Update()
+        {
+            m_resetTimer -= Time.deltaTime;
+
+            if (m_resetTimer < 0) 
+            {
+                _text.text = string.Empty;
+            }
+        }
+
+        #endregion
+
+        #region PRIVATE AND PROTECTED
+
+        private TMP_Text _text;
+
+        #endregion
+    }
+}
