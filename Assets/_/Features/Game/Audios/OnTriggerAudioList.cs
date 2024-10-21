9fc73b403703c4555d001a668cb17ca6edfe016c
Stefan <pokestef@live.be>
2024-10-21 16:50:16 +0200

    Added multiple dialogue reader

---
+++ Assets/_/Features/Game/Audios/OnTriggerAudioList.cs
@@ -0,0 +1,75 @@
+using Data.Runtime;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+namespace Game.Runtime
+{
+    public class OnTriggerAudioList : MonoBehaviour
+    {
+        #region Unity
+
+        private void Update()
+        {
+            if (!_wasPlayed)
+            {
+                if (_startedPlaying && !_isPlaying && _clipIndex < _clips.Count && _audioLength <= 0)
+                {
+                    print(_clipIndex);
+                    AudioPlay(_clipIndex);
+                    _clipIndex += 1;
+                }
+
+                if (_clipIndex == _clips.Count)
+                    _wasPlayed = true;
+            }
+
+            if (_audioLength < 0)
+                _isPlaying = false;
+
+            _audioLength -= Time.deltaTime;
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.layer == LayerMask.NameToLayer("Player") && !_wasPlayed)
+            {
+                _startedPlaying = true;
+            }
+        }
+
+        #endregion
+
+        #region Methods
+
+        public void AudioPlay(int clip)
+        {
+            _audioLength = _clips[clip].m_audio.length + 0.5f;
+            _tmp.GetComponent<TextCleaner>().m_resetTimer = _audioLength;
+            _audio.clip = _clips[clip].m_audio;
+            _audio.Play();
+            _tmp.text = _clips[clip].m_text;
+            _isPlaying = true;
+        }
+
+        #endregion
+
+
+        #region Privates
+
+        [Header ("-- Audio --")]
+        [SerializeField] private List<DialogueScriptableObject> _clips;
+        [SerializeField] private AudioSource _audio;
+
+        [Header("-- Text --")]
+        [SerializeField] private TMP_Text _tmp;
+
+        private bool _wasPlayed;
+        private bool _startedPlaying;
+        private bool _isPlaying;
+        private int _clipIndex;
+        private float _audioLength;
+
+        #endregion
+    }
+}
